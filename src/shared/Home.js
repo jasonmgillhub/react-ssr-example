2e3a782bb9a35ce36d61b195453d58982b0296bf
Jason Gill <jason.gill@stoplight.io>
2022-10-24 11:23:52 -0700

    Latest mess

--- src/shared/Home.js
+++ src/shared/Home.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
 
 export default function Home() {
-	return <h2 className="heading-center">Select a Language</h2>;
+	return ( 
+		<>
+			<h2 className="heading-center">Select a Language</h2>
+		</>
+	);
 }
